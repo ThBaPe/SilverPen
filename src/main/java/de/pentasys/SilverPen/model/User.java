dcc1cfbf1a91405dc6c34606e2e07ac6396b8cce
Björn <bjoern.baumgarten@pentasys.de>
2016-07-19 11:41:33 +0200

    Changed User entity

--- src/main/java/de/pentasys/SilverPen/model/User.java
+++ src/main/java/de/pentasys/SilverPen/model/User.java
@@ -16,12 +16,8 @@ public class User implements Serializable {
 	@Id
 	@Column(unique=true, nullable=false, precision=80)
 	private String email;
-	@Column(unique=true, nullable=false, precision=8)
+	@Column(unique=true, nullable=false, precision=20)
 	private String username;
-	@Column(nullable=false)
-	private String firstName;
-	@Column(nullable=false)
-	private String lastName;
 	@Column(nullable=false)
 	private String password;
 	private static final long serialVersionUID = 1L;
@@ -42,20 +38,6 @@ public class User implements Serializable {
 
 	public void setUsername(String username) {
 		this.username = username;
-	}   
-	public String getFirstName() {
-		return this.firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}   
-	public String getLastName() {
-		return this.lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
 	}   
 	public String getPassword() {
 		return this.password;
