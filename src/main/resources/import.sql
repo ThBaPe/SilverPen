dda20f38074fe11ed8edb0f78e0bafc4c0e9437e
thomas <thomas.bankiel@pentasys.de>
2016-07-22 11:54:07 +0200

    Import.sql mit Beispielbenutzern Passwort = SilverPen

---
+++ src/main/resources/import.sql
@@ -0,0 +1,17 @@
+insert into USER (email, password, username) values ('AK@SilverPen.de', '41200091ea06ec1f59414b4ecbfcf98f', 'Adrian')
+insert into USER (email, password, username) values ('BB@SilverPen.de', 'ec4f44d038e9feac73f5ee255345b90a', 'Björn')
+insert into USER (email, password, username) values ('JE@SilverPen.de', 'f3fc099c8a8d54e55a0c794ef5dd752e', 'Jonas')
+insert into USER (email, password, username) values ('JS@SilverPen.de', '7687ec412a384a4aac225e8a9ead47b0', 'Joachim')
+insert into USER (email, password, username) values ('PM@SilverPen.de', '61bd3bb43feb06b3a3b5ad985c2c864f', 'Polihron')
+insert into USER (email, password, username) values ('PT@SilverPen.de', '4261f35336e9cbbdf81a50d9b096bcc2', 'Patrick')
+insert into USER (email, password, username) values ('TB@SilverPen.de', 'bb7b626c7afe6d205bb30877603f3802', 'Thomas')
+
+
+
+
+
+
+
+
+
+
