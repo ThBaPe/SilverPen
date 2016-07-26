2ddcfb8e0a75aaf96215a46321dc0f5eb1db94df
thomas <thomas.bankiel@pentasys.de>
2016-07-26 10:46:38 +0200

    SystemTest – Projekt hinzugefügt (Selenium) Test für die Registrierung aufgesetzt

---
+++ subproject/SilverPen_ST/src/test/java/de/pentasys/SilverPen_ST/SingUpTest.java
@@ -0,0 +1,119 @@
+package de.pentasys.SilverPen_ST;
+
+import static org.junit.Assert.*;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Sleeper;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SingUpTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    private final String domain = "@Selenium.de";
+    
+    @Before
+    public void setUp() throws Exception {
+      driver = new FirefoxDriver();
+      baseUrl = "http://localhost:8080/";
+      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Durchläuft die Benutzer-Registrierung
+     * @param userName Der zu verwendene Benutzername (ebenfalls präfix für die MailAdd)
+     * @param expected Erwartetes 
+     */
+    private void signupUser(String userName, boolean expected) {
+        driver.get(baseUrl + "/SilverPen/signup.jsf");
+        driver.findElement(By.id("j_idt5:Name")).clear();
+        driver.findElement(By.id("j_idt5:Name")).sendKeys(userName);
+        driver.findElement(By.id("j_idt5:EMail")).clear();
+        driver.findElement(By.id("j_idt5:EMail")).sendKeys(userName + domain);
+        driver.findElement(By.id("j_idt5:pwd1")).clear();
+        driver.findElement(By.id("j_idt5:pwd1")).sendKeys(domain);
+        driver.findElement(By.id("j_idt5:pwd2")).clear();
+        driver.findElement(By.id("j_idt5:pwd2")).sendKeys(domain);
+        driver.findElement(By.id("j_idt5:j_idt10")).click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        
+        String message = driver.findElement(By.id("j_idt5:messages")).getText();
+        assertFalse(message.isEmpty());
+        
+        boolean loginResultOK = message.contains("war erfolgreich");
+        boolean loginResultFailIsReg = message.contains("bereits registriert");
+        
+        // Ist es ein bekannter Zustand?
+        assertTrue(loginResultOK||loginResultFailIsReg);
+
+        assertTrue(expected ? loginResultOK : loginResultFailIsReg);
+    }
+    
+    @Test
+    public void testSignUp() throws Exception {
+
+        String sUserName = UUID.randomUUID().toString().replace("-","");
+        
+        // Erste registrierung
+        signupUser(sUserName, true);
+        
+        // Erneutes registrieren darf nicht gelingen
+        signupUser(sUserName, false);
+        
+      
+    }
+
+    @After
+    public void tearDown() throws Exception {
+      driver.quit();
+      String verificationErrorString = verificationErrors.toString();
+      if (!"".equals(verificationErrorString)) {
+        fail(verificationErrorString);
+      }
+    }
+
+    @SuppressWarnings("unused")
+    private boolean isElementPresent(By by) {
+      try {
+        driver.findElement(by);
+        return true;
+      } catch (NoSuchElementException e) {
+        return false;
+      }
+    }
+
+    @SuppressWarnings("unused")
+    private boolean isAlertPresent() {
+      try {
+        driver.switchTo().alert();
+        return true;
+      } catch (NoAlertPresentException e) {
+        return false;
+      }
+    }
+    
+    @SuppressWarnings("unused")
+    private String closeAlertAndGetItsText() {
+      try {
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        if (acceptNextAlert) {
+          alert.accept();
+        } else {
+          alert.dismiss();
+        }
+        return alertText;
+      } finally {
+        acceptNextAlert = true;
+      }
+    }
+}
