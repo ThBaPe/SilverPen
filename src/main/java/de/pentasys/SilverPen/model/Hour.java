d19c6b82038cdb2f83d3aa39a8977b46b593ae31
thomas <thomas.bankiel@pentasys.de>
2016-07-27 13:08:59 +0200

    EJB Entity: Stundenbuchungen

---
+++ src/main/java/de/pentasys/SilverPen/model/Hour.java
@@ -0,0 +1,70 @@
+package de.pentasys.SilverPen.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="HOUR")
+public class Hour {
+
+    @Id @GeneratedValue 
+    private int id;
+    
+    @Column(nullable=false)
+    private java.sql.Timestamp start;
+
+    @Column(nullable=false)
+    private java.sql.Timestamp stop;
+    
+    @Column
+    private String description;
+
+    @ManyToOne
+    private User user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public java.sql.Timestamp getStart() {
+        return start;
+    }
+
+    public void setStart(java.sql.Timestamp start) {
+        this.start = start;
+    }
+
+    public java.sql.Timestamp getStop() {
+        return stop;
+    }
+
+    public void setStop(java.sql.Timestamp stop) {
+        this.stop = stop;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
+}
