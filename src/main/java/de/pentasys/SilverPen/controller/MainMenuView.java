3464bbaadf297df0aa30e8307fbbb0482be30b32
Björn <bjoern.baumgarten@pentasys.de>
2016-08-02 11:17:29 +0200

    Hauptmenü zur Ansicht implementiert; noch ohne große Funktion

---
+++ src/main/java/de/pentasys/SilverPen/controller/MainMenuView.java
@@ -0,0 +1,39 @@
+package de.pentasys.SilverPen.controller;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class MainMenuView {
+
+    public void upload(){
+        //Sobald Funktion implementiert wird, Message entfernen
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Es existiert kein Hochladen, es existiert nur Zuul!!", null));
+    }
+    
+    public void invoiced(){
+        //Sobald Funktion implementiert wird, Message entfernen
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Noch nicht implementiert!!", null));
+    }
+    
+    public void vacation(){
+        //Sobald Funktion implementiert wird, Message entfernen
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Nope", null));
+    }
+    
+    public void changeUser(){
+      //Sobald Funktion implementiert wird, Message entfernen
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Keine Chance", null));
+    }
+    
+    public void allUsers(){
+      //Sobald Funktion implementiert wird, Message entfernen
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Es existieren keine Benutzer, es existiert nur Zuul!!", null));
+    }
+}
