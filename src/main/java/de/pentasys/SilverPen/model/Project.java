b72a820d7e1534b361d4b506296a9c1a650b4a43
thomas <thomas.bankiel@pentasys.de>
2016-08-09 10:52:20 +0200

    NamedQuery für Project ProjectService listAllProject impl.

--- src/main/java/de/pentasys/SilverPen/model/Project.java
+++ src/main/java/de/pentasys/SilverPen/model/Project.java
@@ -9,17 +9,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 /**
  * Entity implementation class for Entity: Project
  *
  */
+@NamedQueries({ 
+    @NamedQuery(name = "Project.findAll", query = "SELECT p FROM Project p")
+})
 @Entity
 @Table (name="Project")
 public class Project implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    public static final String findAll = "Project.findAll";
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
--- src/main/java/de/pentasys/SilverPen/service/ProjectService.java
+++ src/main/java/de/pentasys/SilverPen/service/ProjectService.java
@@ -1,12 +1,22 @@
 package de.pentasys.SilverPen.service;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+
+import de.pentasys.SilverPen.model.Project;
+import de.pentasys.SilverPen.model.Role;
 
 @Stateless
 public class ProjectService {
 
     @Inject
     EntityManager em;
+    
+    
+    public List<Project> listAllProject() {
+        return em.createNamedQuery(Project.findAll,Project.class).getResultList();
+   }
 }
