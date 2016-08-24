398f50ce0808e5af9310a66b8196774831108b80
Nils <nils_block@gmx.net>
2016-08-24 14:40:08 +0200

    Daten werden aus der DB geholt und im DataTable dargestellt. Daten müssen noch verarbeitet werden.

--- src/main/java/de/pentasys/SilverPen/controller/BookingItemListView.java
+++ src/main/java/de/pentasys/SilverPen/controller/BookingItemListView.java
@@ -1,6 +1,7 @@
 package de.pentasys.SilverPen.controller;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
@@ -8,24 +9,27 @@ import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import de.pentasys.SilverPen.model.User;
 import de.pentasys.SilverPen.model.booking.BookingItem;
 import de.pentasys.SilverPen.service.BookingItemListService;
+import de.pentasys.SilverPen.service.LoginInfo;
 
 @Named
 @ManagedBean
 @RequestScoped
 public class BookingItemListView {
     
-    @Inject
-    BookingItemListService bils;
-    User user;
+    @Inject BookingItemListService bils;
+    @Inject private LoginInfo curLogin;
+    
+    @Inject private Logger lg;
 
     private List<BookingItem> bookingItems;
     
     @PostConstruct
     public void init(){
-        setBookingItems(bils.getBookingItems(user));
+        setBookingItems(bils.getBookingItems(curLogin.getCurrentUser()));
+        lg.info("hi");
+        lg.info("Anzahl B-Items: " + getBookingItems().size());
     }
 
 	public List<BookingItem> getBookingItems() {
